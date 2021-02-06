add06c4d6e55e0e36421f5d036a47b7860683f5f
boog25 <voroshilov.d.v123@gmail.com>
2021-02-06 21:03:44 +0500

    changes files

---
+++ src/main/java/ru/netology/manager/MovieManager.java
@@ -0,0 +1,44 @@
+package ru.netology.manager;
+
+import ru.netology.domain.Movie;
+
+public class MovieManager {
+    private Movie[] items = new Movie[0];
+    private int afishaLength = 10;
+
+    public MovieManager() {
+    }
+
+    public void add(Movie item) {
+        int length = items.length + 1;
+        Movie[] tmp = new Movie[length];
+        System.arraycopy(items, 0, tmp, 0, items.length);
+        int lastIndex = tmp.length - 1;
+        tmp[lastIndex] = item;
+        items = tmp;
+    }
+
+    public Movie[] getAll() {
+        Movie[] result = new Movie[Math.min(afishaLength, items.length)];
+        for (int i = 0; i < Math.min(afishaLength, items.length); i++) {
+            int index = Math.min(afishaLength, items.length) - i - 1;
+            result[i] = items[index];
+        }
+        return result;
+    }
+
+
+    public MovieManager(int afishaLength) {
+        this.afishaLength = afishaLength;
+    }
+
+    public Movie[] getAllLastItems() {
+        Movie[] result = new Movie[Math.min(afishaLength, items.length)];
+        for (int i = 0; i < result.length; i++) {
+            int index = items.length - i - 1;
+            result[i] = items[index];
+
+        }
+        return result;
+    }
+}
--- src/test/java/ru/netology/manager/MovieTest.java
+++ src/test/java/ru/netology/manager/MovieTest.java
@@ -1,148 +1,70 @@
 package ru.netology.manager;
 
-
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import ru.netology.domain.Movie;
 
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 
+public class MovieTest {
 
-class MovieTest {
-    PosterManager movieManager = new PosterManager();
+    MovieManager movieManager = new MovieManager();
 
-    @Test
-    void MoviesNoAdd() {
-        movieManager.getAll();
-        Movie[] expected = new Movie[0];
-        Movie[] actual = movieManager.getAll();
-        assertArrayEquals(expected, actual);
+    Movie item1 = new Movie(1, "Монгол", "История");
+    Movie item2 = new Movie(2, "Отель Белград", "Фильм");
+    Movie item3 = new Movie(3, "Один дома", "комедия");
+    Movie item4 = new Movie(4, "Бладшот", "боевик");
+    Movie item5 = new Movie(5, "Остров проклятых", "ужасы");
+    Movie item6 = new Movie(6, "Простоквашино", "мультфильм");
+    Movie item7 = new Movie(7, "Борад", "комедия");
+    Movie item8 = new Movie(8, "Интерстеллер", "фантастика");
+    Movie item9 = new Movie(9, "Доктор Кто", "сериал");
+    Movie item10 = new Movie(10, "Фиксики", "мультфильм");
+    Movie item11 = new Movie(11, "Фиксики2", "мультфильм");
+
+    @BeforeEach
+    void setup() {
+        movieManager.add(item1);
+        movieManager.add(item2);
+        movieManager.add(item3);
+        movieManager.add(item4);
+        movieManager.add(item5);
+        movieManager.add(item6);
+        movieManager.add(item7);
+        movieManager.add(item8);
+        movieManager.add(item9);
     }
 
     @Test
-    void addMovie() {
-        Movie one = new Movie(1, "Монгол", "История");
-        movieManager.addMovie(one);
-        movieManager.getAll();
-        Movie[] expected = new Movie[]{one};
+    public void shouldAddMoviesEqualManagerLength() {
+        movieManager.add(item10);
         Movie[] actual = movieManager.getAll();
+        Movie[] expected = new Movie[]{item10, item9, item8, item7, item6, item5, item4, item3, item2, item1};
         assertArrayEquals(expected, actual);
+    }
+
+    MovieManager manager1 = new MovieManager(5);
+
+    @BeforeEach
+    void setup1() {
+        manager1.add(item1);
+        manager1.add(item2);
+        manager1.add(item3);
+        manager1.add(item4);
+        manager1.add(item5);
+        manager1.add(item6);
+        manager1.add(item7);
+        manager1.add(item8);
+        manager1.add(item9);
+
     }
 
     @Test
-    void addThenMovie() {
-        Movie one = new Movie(1, "Монгол", "История");
-        Movie two = new Movie(2, "Отель Белград", "Фильм");
-        Movie three = new Movie(3, "Один дома", "комедия");
-        Movie four = new Movie(4, "Бладшот", "боевик");
-        Movie five = new Movie(5, "Остров проклятых", "ужасы");
-        Movie six = new Movie(6, "Простоквашено", "мультфильм");
-        Movie seven = new Movie(7, "Борад", "комедия");
-        Movie eight = new Movie(8, "Интерстеллер", "фантастика");
-        Movie nine = new Movie(9, "Доктор Кто", "сериал");
-        Movie then = new Movie(10, "Фиксики", "мультфильм");
-        movieManager.addMovie(one);
-        movieManager.addMovie(two);
-        movieManager.addMovie(three);
-        movieManager.addMovie(four);
-        movieManager.addMovie(five);
-        movieManager.addMovie(six);
-        movieManager.addMovie(seven);
-        movieManager.addMovie(eight);
-        movieManager.addMovie(nine);
-        movieManager.addMovie(then);
-        movieManager.getAll();
-        Movie[] actual = movieManager.getAll();
-        Movie[] expected = new Movie[]{then, nine, eight, seven, six, five, four, three, two, one};
+    public void shouldAddLastItems() {
+        manager1.add(item10);
+        Movie[] actual = manager1.getAllLastItems();
+        Movie[] expected = new Movie[]{item10, item9, item8, item7, item6};
         assertArrayEquals(expected, actual);
     }
 
-
-    @Test
-    void addMoreThenMovie() {
-        Movie one = new Movie(1, "Монгол", "История");
-        Movie two = new Movie(2, "Отель Белград", "Фильм");
-        Movie three = new Movie(3, "Один дома", "комедия");
-        Movie four = new Movie(4, "Бладшот", "боевик");
-        Movie five = new Movie(5, "Остров проклятых", "ужасы");
-        Movie six = new Movie(6, "Простоквашено", "мультфильм");
-        Movie seven = new Movie(7, "Борад", "комедия");
-        Movie eight = new Movie(8, "Интерстеллер", "фантастика");
-        Movie nine = new Movie(9, "Доктор Кто", "сериал");
-        Movie then = new Movie(10, "Фиксики", "мультфильм");
-        Movie eleven = new Movie(11, "Фиксики2", "мультфильм");
-        movieManager.addMovie(one);
-        movieManager.addMovie(two);
-        movieManager.addMovie(three);
-        movieManager.addMovie(four);
-        movieManager.addMovie(five);
-        movieManager.addMovie(six);
-        movieManager.addMovie(seven);
-        movieManager.addMovie(eight);
-        movieManager.addMovie(nine);
-        movieManager.addMovie(then);
-        movieManager.addMovie(eleven);
-        movieManager.getAll();
-        Movie[] actual = movieManager.getAll();
-        Movie[] expected = new Movie[]{eleven, then, nine, eight, seven, six, five, four, three, two};
-        assertArrayEquals(expected, actual);
-    }
-
-    @Test
-    void shouldUseNoArgsConstructor() {
-        movieManager.setCount(5);
-        Movie one = new Movie(1, "Монгол", "История");
-        Movie two = new Movie(2, "Отель Белград", "Фильм");
-        Movie three = new Movie(3, "Один дома", "комедия");
-        Movie four = new Movie(4, "Бладшот", "боевик");
-        Movie five = new Movie(5, "Остров проклятых", "ужасы");
-        Movie six = new Movie(6, "Простоквашено", "мультфильм");
-        Movie seven = new Movie(7, "Борад", "комедия");
-        Movie eight = new Movie(8, "Интерстеллер", "фантастика");
-        Movie nine = new Movie(9, "Доктор Кто", "сериал");
-        Movie then = new Movie(10, "Фиксики", "мультфильм");
-        movieManager.addMovie(one);
-        movieManager.addMovie(two);
-        movieManager.addMovie(three);
-        movieManager.addMovie(four);
-        movieManager.addMovie(five);
-        movieManager.addMovie(six);
-        movieManager.addMovie(seven);
-        movieManager.addMovie(eight);
-        movieManager.addMovie(nine);
-        movieManager.addMovie(then);
-        movieManager.getAll();
-        Movie[] actual = movieManager.getAll();
-        Movie[] expected = new Movie[]{then, nine, eight, seven, six};
-        assertArrayEquals(expected, actual);
-    }
-
-
-    @Test
-    void shouldGetFiveMovie() {
-        PosterManager movieManager = new PosterManager(5);
-        Movie one = new Movie(1, "Монгол", "История");
-        Movie two = new Movie(2, "Отель Белград", "Фильм");
-        Movie three = new Movie(3, "Один дома", "комедия");
-        Movie four = new Movie(4, "Бладшот", "боевик");
-        Movie five = new Movie(5, "Остров проклятых", "ужасы");
-        Movie six = new Movie(6, "Простоквашено", "мультфильм");
-        Movie seven = new Movie(7, "Борад", "комедия");
-        Movie eight = new Movie(8, "Интерстеллер", "фантастика");
-        Movie nine = new Movie(9, "Доктор Кто", "сериал");
-        Movie then = new Movie(10, "Фиксики", "мультфильм");
-        movieManager.addMovie(one);
-        movieManager.addMovie(two);
-        movieManager.addMovie(three);
-        movieManager.addMovie(four);
-        movieManager.addMovie(five);
-        movieManager.addMovie(six);
-        movieManager.addMovie(seven);
-        movieManager.addMovie(eight);
-        movieManager.addMovie(nine);
-        movieManager.addMovie(then);
-        movieManager.getAll();
-        Movie[] actual = movieManager.getAll();
-        Movie[] expected = new Movie[]{then, nine, eight, seven, six};
-        assertArrayEquals(expected, actual);
-    }
 }
